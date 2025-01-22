0469476f26153719f34362685cfba6a9197f8d37
Andrey Araujo <andreyaraujo.dev@gmail.com>
2025-01-22 19:27:37 -0300

    feat: add categories resource

---
+++ fincheck-api/src/modules/categories/categories.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { CategoriesRepository } from '../../shared/database/repositories/categories.repository';
+
+@Injectable()
+export class CategoriesService {
+  constructor(private readonly categoriesRepository: CategoriesRepository) {}
+
+  async findAllByUserId(userId: string) {
+    return this.categoriesRepository.findMany({ where: { userId: userId } });
+  }
+}
