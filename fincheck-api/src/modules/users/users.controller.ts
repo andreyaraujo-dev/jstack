22644fdf61edc3a174edd911f649c8b468e5523f
Andrey Araujo <andreyaraujo.dev@gmail.com>
2025-01-21 22:07:58 -0300

    feat: add active user id decorator

--- fincheck-api/src/modules/users/users.controller.ts
+++ fincheck-api/src/modules/users/users.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ActiveUserId } from '../../shared/decorators/active-user-id';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('me')
-  async me(@Req() request: any) {
-    return this.usersService.getUserById(request.userId);
+  async me(@ActiveUserId() userId: string) {
+    return this.usersService.getUserById(userId);
   }
 }
